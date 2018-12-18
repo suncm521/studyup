62d1b0613e0cb786a044e02736a496b054205cbf
suncm <731966510@qq.com>
2018-12-18 11:36:43 +0800

    maven+ssm

---
+++ src/main/web/java1/dao/BaseMapper.java
@@ -0,0 +1,8 @@
+package java1.dao;
+
+/**
+ * Created by dell、 on 2018/12/6.
+ */
+public interface BaseMapper<T> {
+
+}
---
+++ src/main/web/java1/entity/Person.java
@@ -0,0 +1,69 @@
+package java1.entity;
+
+import java.io.Serializable;
+import java.util.Date;
+
+public class Person implements Serializable {
+    private Integer id;
+
+    private String name;
+
+    private Integer age;
+
+    private Date birthday;
+
+    private String image;
+
+    private static final long serialVersionUID = 1L;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name == null ? null : name.trim();
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public Date getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(Date birthday) {
+        this.birthday = birthday;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image == null ? null : image.trim();
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                ", birthday=" + birthday +
+                ", image='" + image + '\'' +
+                '}';
+    }
+}
